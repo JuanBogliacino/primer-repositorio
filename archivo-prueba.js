0a16afc679a74d399081b8c28a70e2db7e08d07f
JuanBogliacino <juanboglia12@gmail.com>
2022-05-18 21:14:05 -0300

    rober gay agregado

--- archivo-prueba.js
+++ archivo-prueba.js
@@ -5,3 +5,9 @@ console.log(sexo);
 let messi = "GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOD";
 
 console.log(messi);
+
+let rober = "GAY";
+
+if(rober == "GAY"){
+     console.log("Bober Gaaaaaaaayyyy");
+}
