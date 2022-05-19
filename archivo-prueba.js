0c9f89788c2c50bbb1355e67dc18dc3c02818744
JuanBogliacino <juanboglia12@gmail.com>
2022-05-19 18:41:01 -0300

    nashe

--- archivo-prueba.js
+++ archivo-prueba.js
@@ -1,21 +1,3 @@
-let sexo = "solo gay";
+let nashe = "NASHEEE";
 
-console.log(sexo);
-
-let messi = "GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOD";
-
-console.log(messi);
-
-let rober = "GAY";
-
-if(rober == "GAY"){
-     console.log("Bober Gaaaaaaaayyyy");
-}
-
-let futbolistasGod = ["Messi", "Neymar DO Santos", "KAKÁ", "Roman(el ultimo 10)", "Titi Henry", "deibis berkam"]
-
-console.log(futbolistasGod);
-
-let AndrewGarfield = "THE MOST AMAZING SPIDERMAN OF ALL TIME"
-
-console.log(AndrewGarfield);
+console.log(nashe);
